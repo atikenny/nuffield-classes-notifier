457a50f1e3d630c3cafdaab3c3d4304dd2ae996e
Attila Bartha <atikenny@gmail.com>
2018-01-13 13:46:13 +0000

    create logger and navigator functions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
 const config = {
     screenshotsDir: 'screenshots',
     loginPage: 'https://member.nuffieldhealth.com/bookings/login.asp'
 };
 
-(async () => {
+async function getFlowLogger({ screenshotsDir, page, fullPage = true }) {
+    let step = 0;
+    const flowId = Date.now();
+    const flowScreenshotsDir = `./${screenshotsDir}/${flowId}`;
+
+    if (!(await fs.exists(flowScreenshotsDir))) {
+        fs.mkdir(flowScreenshotsDir);
+    }
+
+    return async function log() {
+        await page.screenshot({
+            path: `${flowScreenshotsDir}/step-${++step}.png`,
+            fullPage
+        });
+    };
+}
+
+function getNavigator({ page, log }) {
+    return async function navigateToPage(url) {
+        await page.goto(url);
+        await log();
+    }
+}
+
+(async function main({ screenshotsDir, loginPage }) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    await page.goto(config.loginPage);
-    await page.screenshot({
-        path: `${config.screenshotsDir}/login.png`
+    const log = await getFlowLogger({
+        screenshotsDir,
+        page
     });
+    const navigateToPage = getNavigator({ page, log });
+
+    // LOGIN
+    await navigateToPage(loginPage);
 
     await browser.close();
-})();
+})(config);
