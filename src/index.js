2d2eba4163e48405f7a410148655b495074bb66c
Attila Bartha <atikenny@gmail.com>
2018-01-13 13:20:33 +0000

    ignore screenshots dir, add login page navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('start');
+const puppeteer = require('puppeteer');
+const config = {
+    screenshotsDir: 'screenshots',
+    loginPage: 'https://member.nuffieldhealth.com/bookings/login.asp'
+};
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(config.loginPage);
+    await page.screenshot({
+        path: `${config.screenshotsDir}/login.png`
+    });
+
+    await browser.close();
+})();
